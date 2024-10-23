318b57e732017585c4fd0a506b28d803c50ce1e4
ДимДим <1000rub@list.ru>
2024-10-23 15:51:54 +0300

    cont DZ6

--- DZ6Main.c
+++ DZ6Main.c
@@ -5,21 +5,23 @@ char oneWrite[] = "1 - Output 0-9 While cycling";
 char twoWrite[] = "2 - Tabl Pythagoras";
 char threeWrite[] = "3 - Numbers in that sequence";
 char zeroWrite[] = "0 - Stop run DZ6";
+int argI = 9; // по заданию значение 9
 //
 void menuConsole();
 void whileCycling();
 void tablPythagoras();
 void numberSequence();
+void printInDataConsole(int volume[], int size);
 //
 int main()
 {
+    printf("Executer DZ6\n\n");
     menuConsole();
     return 0;
 }
 //
 void menuConsole()
 {
-    printf("Executer DZ6\n\n");
     printf("%s\n%s\n%s\n%s\n\n", oneWrite, twoWrite, threeWrite, zeroWrite);
 
     char pressButton;
@@ -47,17 +49,61 @@ void menuConsole()
         break;
     }
 
-     menuConsole();
+    menuConsole();
 }
 void whileCycling()
 {
-
+    while (argI >= 0)
+    {
+        printf("iteration while argument = %i\n", argI);
+        argI--;
+    }
 }
 void tablPythagoras()
 {
-
+    for (int i = 1; i < 10; i++)
+    {
+        for (int y = 1; y < 10; y++)
+        {
+            printf("%i x %i = %i\n", i, y, i * y);
+        }
+        printf("\n");
+    }
 }
 void numberSequence()
 {
+    int const size=5;
+    int inVolume;
+    int volume[size] = {};
+    int volumeSequence[size]={};
 
+    while (inVolume != 0)
+    {
+        scanf("%d", &inVolume);
+        for (int i = 0; i < size; i++)
+        {
+            if (volume[i] == 0)
+            {
+                volume[i] = inVolume;
+                printInDataConsole(volume, size);
+                break;
+            }
+        }
+        
+        //for()
+
+
+    }
+}
+void printInDataConsole(int volume[], int size)
+{
+    printf("in data -> ");
+    for (int i = 0; i < size; i++)
+    {
+        if (volume[i] != 0)
+        {
+            printf("%i ", volume[i]);
+        }
+    }
+    printf("\n");
 }
